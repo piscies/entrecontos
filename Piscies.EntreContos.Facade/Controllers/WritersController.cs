4ed0e27daaba4a0e946579deb2468058c01756a6
Marco Saraiva <maasaraiva@firjan.com.br>
2019-01-21 17:25:28 -0200

    Finished a full example of running code with WritersController.

--- Piscies.EntreContos.Facade/Controllers/WritersController.cs
+++ Piscies.EntreContos.Facade/Controllers/WritersController.cs
@@ -3,17 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Piscies.Common.Crosscut.DTO;
+using Piscies.EntreContos.Application.Interface;
+using Piscies.EntreContos.Crosscut.DTO;
 
 namespace Piscies.EntreContos.Facade.Controllers
 {
     [Route("api/[controller]")]
     public class WritersController : Controller
     {
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+        IWriterApp writerApp;
+
+        public WritersController(IWriterApp writerApp)
         {
-            return new string[] { "value1", "value2" };
+            this.writerApp = writerApp;
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        {
+            ActionResponseDTO response = new ActionResponseDTO();
+
+            try
+            {
+                response = writerApp.List();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+
+            return Ok(response.Content);
         }
 
         // GET api/values/5
