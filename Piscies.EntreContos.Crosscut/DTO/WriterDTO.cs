1739f054fc7008d2908a5e9579c593246a35f9b4
Marco Saraiva <maasaraiva@firjan.com.br>
2019-01-21 17:11:51 -0200

    Creating the Crosscut layer

---
+++ Piscies.EntreContos.Crosscut/DTO/WriterDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Piscies.EntreContos.Crosscut.DTO
+{
+    public class WriterDTO
+    {
+        public string Name { get; set; }
+    }
+}
