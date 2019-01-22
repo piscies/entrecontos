678f6d7b398af9c75583d5236ee9a3c7a8e5b7f9
Marco Saraiva <maasaraiva@firjan.com.br>
2019-01-22 15:26:42 -0200

    Adding ConnectionStrings "secret"

--- Piscies.EntreContos.Facade/Startup.cs
+++ Piscies.EntreContos.Facade/Startup.cs
@@ -34,6 +34,9 @@ namespace Piscies.EntreContos.Facade
 
             //Infrastructure
             services.AddScoped<IWriterInfrastructure, WriterInfrastructure>();
+
+            //Configurations
+            services.AddSingleton<IConfiguration>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- Piscies.EntreContos.Infrastructure/WriterInfrastructure.cs
+++ Piscies.EntreContos.Infrastructure/WriterInfrastructure.cs
@@ -1,4 +1,5 @@
-﻿using Piscies.EntreContos.Domain;
+﻿using Microsoft.Extensions.Configuration;
+using Piscies.EntreContos.Domain;
 using Piscies.EntreContos.Infrastructure.Interface;
 using System;
 using System.Collections.Generic;
@@ -9,11 +10,18 @@ namespace Piscies.EntreContos.Infrastructure
 {
     public class WriterInfrastructure : IWriterInfrastructure
     {
+        private IConfiguration Configuration { get; set; }
+
+        public WriterInfrastructure(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
         public IList<Writer> List()
         {
             List<Writer> allWriters;
 
-            using (EntityModelContext context = new EntityModelContext())
+            using (EntityModelContext context = new EntityModelContext(Configuration))
             {
                 allWriters = context.Writer.ToList();
             }
