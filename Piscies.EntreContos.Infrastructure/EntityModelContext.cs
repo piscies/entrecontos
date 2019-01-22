f5b0e011e7e8aa63c9ca0c8f65f2aae036755605
Marco Saraiva <maasaraiva@firjan.com.br>
2019-01-22 15:57:04 -0200

    Adding the EntityModelContext

---
+++ Piscies.EntreContos.Infrastructure/EntityModelContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Piscies.EntreContos.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Piscies.EntreContos.Infrastructure
+{
+    public class EntityModelContext : DbContext
+    {
+        private IConfiguration Configuration { get; set; }
+
+        public EntityModelContext(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(Configuration.GetConnectionString("mySqlAWS"));
+        }
+
+        public DbSet<Writer> Writer { get; set; }
+    }
+}
