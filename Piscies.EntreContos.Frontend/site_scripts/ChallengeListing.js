99c6a480ab0e2929687ffe1c53dcffc0dfc1d63f
Marco Saraiva <maasaraiva@firjan.com.br>
2019-02-01 17:07:52 -0200

    Creating  the frontend.

---
+++ Piscies.EntreContos.Frontend/site_scripts/ChallengeListing.js
@@ -0,0 +1,12 @@
+﻿function GetChallengesFromApi() {
+    REST_Get('challenges', ReadChallengesFromApiAsync);
+}
+
+function ReadChallengesFromApiAsync() {
+    var data = JSON.parse(this.response);
+
+    data.forEach(challenge => {
+        // Log each movie's title
+        console.log(challenge.Theme);
+    });
+}
---
+++ Piscies.EntreContos.Frontend/site_scripts/RESTRequests.js
@@ -0,0 +1,22 @@
+﻿var APIServiceBaseAddress = 'http://localhost:64741/api/'
+
+function REST_Get(apiPath, jsonDataInterpretationCallback) {
+
+    var request = new XMLHttpRequest();
+
+    // Open a new connection, using the GET request on the URL endpoint
+    request.open('GET', APIServiceBaseAddress + apiPath, true);
+
+    //Reads JSON Data
+    request.onload = function () {
+        var data = JSON.parse(this.response);
+
+        data.forEach(movie => {
+            // Log each movie's title
+            console.log(movie.title);
+        });
+    }    
+
+    // Send request
+    request.send();
+}
