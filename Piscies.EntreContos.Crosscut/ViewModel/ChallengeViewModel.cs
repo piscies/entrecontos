766bea28b9d87f3bb6b635acbab34556a843243d
Marco <maruko.sensei@gmail.com>
2019-09-10 16:47:46 -0300

    Adding challenge view

---
+++ Piscies.EntreContos.Crosscut/ViewModel/ChallengeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Piscies.EntreContos.Crosscut.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Piscies.EntreContos.Crosscut.ViewModel
+{
+    public class ChallengeViewModel
+    {
+        public List<ChallengeDTO> Challenges { get; set; }
+    }
+}
---
+++ Piscies.EntreContos.Frontend.MVC/Controllers/ChallengesController.cs
@@ -0,0 +1,25 @@
+﻿using Piscies.EntreContos.Crosscut.DTO;
+using Piscies.EntreContos.Crosscut.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Piscies.EntreContos.Frontend.MVC.Controllers
+{
+    public class ChallengesController : Controller
+    {   
+        public ActionResult Index()
+        {
+            List<ChallengeDTO> challenges = new List<ChallengeDTO>();
+            challenges.Add(new ChallengeDTO() { Theme = "Cemitérios", AnnouncementDate = DateTime.Now.AddMonths(3) });
+            challenges.Add(new ChallengeDTO() { Theme = "Fantasmas", AnnouncementDate = DateTime.Now.AddMonths(6) });
+            challenges.Add(new ChallengeDTO() { Theme = "Faroeste", AnnouncementDate = DateTime.Now.AddMonths(-7) });
+
+            ChallengeViewModel challengeViewModel = new ChallengeViewModel();
+            challengeViewModel.Challenges = challenges;
+            return View(challengeViewModel);
+        }
+    }
+}
